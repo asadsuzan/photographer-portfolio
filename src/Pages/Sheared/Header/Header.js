5976bb4f5e511a4f745521ac2f0b7791df7b9ee3
suzan <asadsuzan7@gmail.com>
2022-04-17 12:16:38 +0600

    set nav bar at header page

--- src/Pages/Sheared/Header/Header.js
+++ src/Pages/Sheared/Header/Header.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>Legend</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav
+            className="ms-auto fs-6 align-items-center"
+            style={{ gap: "20px" }}
+          >
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/blog">
+              Blog
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
