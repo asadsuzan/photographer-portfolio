de1e142de80abee8f366b1675270c94fca3bf83b
suzan <asadsuzan7@gmail.com>
2022-04-17 12:02:16 +0600

    initialize pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Login from "./Pages/Authentication/Loging/Login";
+import Signup from "./Pages/Authentication/Signup/Signup";
+import Blog from "./Pages/Blog/Blog";
+import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Sheared/Footer/Footer";
+import Header from "./Pages/Sheared/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
   );
 }
 
